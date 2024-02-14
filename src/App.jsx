7123ccd0eb951ec0bf7a3006b087b29543759485
AliTayyab981 <maliktayyab12344321@gmail.com>
2024-02-14 19:30:38 +0500

    Ready Sigin Signip With Form Handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,8 @@
 import './App.css'
-import Signin from './Auth/Pages/Signin';
-import Signup from './Auth/Pages/Signup'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import TermsOfUse from './Rule/TermsOfUse';
-import PrivacyPolicy from './Rule/PrivacyPolicy';
+import {RouterProvider} from "react-router-dom";
+import { router } from './RoutingPages/Routepages';
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/signup",
-      element: <Signup/> ,
-    },
-    {
-      path: "/login",
-      element: <Signin/> ,
-    },
-    {
-      path: "/signup/termofuse",
-      element: <TermsOfUse/> ,
-    },
-    {
-      path: "/signup/privacypolicy",
-      element: <PrivacyPolicy/> ,
-    },
-    {
-      path: "/login/termofuse",
-      element: <TermsOfUse/> ,
-    },
-    {
-      path: "/login/privacypolicy",
-      element: <PrivacyPolicy/> ,
-    },
-  ]);
-
+  
   return (
 
     <>
