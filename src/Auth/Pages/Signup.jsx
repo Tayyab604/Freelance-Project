2da7a2a9dd18707618c1168c1e873e113e8f1be0
AliTayyab981 <maliktayyab12344321@gmail.com>
2024-02-14 19:22:53 +0500

    Signup Sigin With FromHandling Ready

--- src/Auth/Pages/Signup.jsx
+++ src/Auth/Pages/Signup.jsx
@@ -1,7 +1,9 @@
-import { Box, Button, Flex, FormLabel, Image, Input, Stack, Text } from '@chakra-ui/react'
+import { Box, Button, Flex, FormControl, FormErrorMessage, FormLabel, Image, Input, Stack, Text } from '@chakra-ui/react'
+import { Field, Form, Formik } from 'formik'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
+import { signupFormValidation } from './Validation/Validationj'
 
 const Signup = () => {
   return (
@@ -13,6 +15,17 @@ const Signup = () => {
        </Flex>
       </BgImg>
       <Box  flexGrow="1" >
+        <Formik
+        initialValues={{
+          username : "",
+          email : "",
+          password : ""
+        }}
+        onSubmit={(values)=>{
+        console.log(values)
+        }}
+        validationSchema={signupFormValidation}
+        >
         <Stack   justifyContent="center"  h="100%" alignItems="center" >
         <Stack  gap="30px"  >
         <Box>
@@ -20,36 +33,50 @@ const Signup = () => {
         Create an account
         </Text>
         </Box>
+        <Form>
         <Stack gap="20px" >
-        <Box>
-        <FormLabel color="#333333" >
-        User name
-        </FormLabel>
-        <Input borderRadius="12px"   border="1px solid#333333" type='text' w="473px" />
-        </Box>
-        <Box>
-        <FormLabel color="#333333" >
-        Email address
-        </FormLabel>
-        <Input borderRadius="12px" border="1px solid#333333" type='email' w="473px" />
-        </Box>
-        <Box>
-        <FormLabel color="#333333" >
-        Password
-        </FormLabel>
-        <Input borderRadius="12px" border="1px solid#333333" type='password' w="473px" />
-        </Box>
+        <Field name='username' >
+                {({ field, meta }) =>
+                  <FormControl isInvalid={!!(meta.error)} >
+                    <FormLabel color="#333333" htmlFor="username"   >Username</FormLabel>
+                    <Input   borderRadius="12px" border="1px solid#333333" type='text' w="473px" {...field} name='username' />
+                    <FormErrorMessage>{meta.error}</FormErrorMessage>
+                  </FormControl>
+                }
+        </Field>
+        <Field name='email' >
+                {({ field, meta }) =>
+                  <FormControl isInvalid={!!(meta.error)} >
+                    <FormLabel color="#333333" htmlFor="email"   >Email</FormLabel>
+                    <Input   borderRadius="12px" border="1px solid#333333" type='email' w="473px" {...field} name='email' />
+                    <FormErrorMessage>{meta.error}</FormErrorMessage>
+                  </FormControl>
+                }
+        </Field>
+        <Field name='password' >
+                {({ field, meta }) =>
+                  <FormControl isInvalid={!!(meta.error)} >
+                    <FormLabel color="#333333" htmlFor="password"   >Password</FormLabel>
+                    <Input    borderRadius="12px" border="1px solid#333333" type='password' w="473px" {...field} name='password' />
+                    <FormErrorMessage>{meta.error}</FormErrorMessage>
+                  </FormControl>
+                }
+        </Field>
+         </Stack>
+     <Stack mt="20px" gap="20px" >
+     <Text color="#333333" >By creating an account, you agree to our <br /><Link to="termofuse" ><Box as={"span"} fontWeight="bold"cursor="pointer" color="#570880" >Terms of use</Box></Link> and <Link to="privacypolicy">
+      <Box as={"span"} cursor="pointer" fontWeight="bold" color="#570880" >Privacy Policy</Box></Link>  </Text>
+      <Stack>
+      <Button  type="submit" _hover={{
+        backgroundColor : "#69268a"
+      }} w="270px" h="64px" fontSize="22px" borderRadius="32px" backgroundColor="#570880" color="white"  >Signup</Button>
+     </Stack>
+      <Text>Already have an ccount?   <Link to="/login" ><Box as={"span"} fontWeight="bold" color="#570880" cursor="pointer" >Login  </Box></Link>  </Text>
+      </Stack>
+      </Form>
         </Stack>
-        <Text color="#333333" >By creating an account, you agree to our <br /><Link to="termofuse" ><Box as={"span"} fontWeight="bold"cursor="pointer" color="#570880" >Terms of use</Box></Link> and <Link to="privacypolicy">
-        <Box as={"span"} cursor="pointer" fontWeight="bold" color="#570880" >Privacy Policy</Box></Link>  </Text>
-        <Stack>
-        <Button _hover={{
-          backgroundColor : "#69268a"
-        }} w="270px" h="64px" fontSize="22px" borderRadius="32px" backgroundColor="#570880" color="white"  >Create an accrount</Button>
-        <Text>Already have an ccount? <Link to="/login" ><Box as={"span"} fontWeight="bold" color="#570880" cursor="pointer" >Log in</Box></Link>  </Text>
         </Stack>
-        </Stack>
-        </Stack>
+        </Formik>
       </Box  >
     </Flex>
     
---
+++ src/Auth/Pages/Validation/Validationj.js
@@ -0,0 +1,11 @@
+import { object, string } from 'yup'
+
+export const signupFormValidation = object({
+  username: string().required("Username is Required").min(3, "Username must be at least 3 character").max(20, "username must be lower 20 character"),
+  email: string().email("Email is Invalid").required("Email is Required"),
+  password: string().min(8, "Password must be at least 8 character").max(20, "Password must be lower 20 character").required("Password is Required"),
+})
+export const signinFormValidation = object({
+  email: string().email("Email is Invalid").required("Email is Required"),
+  password: string().min(8, "Password must be at least 8 character").max(20, "Password must be lower 20 character").required("Password is Required"),
+})
