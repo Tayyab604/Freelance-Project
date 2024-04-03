8e918b8edfd700c4fb678d556fe4ac3ad3b47568
AliTayyab981 <maliktayyab12344321@gmail.com>
2024-04-03 09:27:48 +0500

    Start Working On FrontEnd

--- Client-Side/src/Auth/Validation/Validationj.js
+++ Client-Side/src/Auth/Validation/Validationj.js
@@ -19,3 +19,35 @@ export const UpdateforgetPasswordValidation = object({
 export const CatagoryValidation = object({
   catagory : string().required("Skill is Required"),
 })
+export const ProfileBasicInfoValidation = object({
+  firstname: string().required("FirstName is Required").min(3, "Must be at least 3 character").max(10, "Must be lower 10 character"),
+  lastname: string().required("LastName is Required").min(3, "Must be at least 3 character").max(10, "Must be lower 10 character"),
+  sex: string().required("Gender is Required"),
+  dob: string().required("DateofBirth is Required"),
+  city: string().required("City is Required").min(3, "Must be at least 3 character").max(20, "Must be lower 20 character"),
+  address: string().required("Address is Required").min(3, "Must be at least 3 character").max(50, "Must be lower 50 character"),
+  title: string().required("Title is Required").min(20, "Title must be at least 20 character").max(150, "Title must be lower 150 character"),
+  primaryskill: string().required("Skill is Required"),
+  aboutyourself : string().required("About YourSelf is Required").min(250, "Must be at least 250 character").max(1200, "Must be lower 1200 character"),
+})
+export const ProfileWorkExperienceValidation = object({
+  position: string().required("Position is Required").min(10, "Must be at least 10 character").max(50, "Must be lower 50 character"),
+  company: string().required("Company is Required").min(20, "Must be at least 20 character").max(120, "Must be lower 120 character"),
+  startdata: string().required("StartDate is Required"),
+  enddata: string().required("EndDate is Required"),
+  summary : string().required("Summary is Required").min(250, "Must be at least 250 character").max(1200, "Must be lower 1200 character"),
+})
+export const ProfileEducationValidation = object({
+  country: string().required("Country is Required").min(10, "Must be at least 10 character").max(50, "Must be lower 50 character"),
+  collage: string().required("Collage is Required").min(20, "Must be at least 20 character").max(120, "Must be lower 120 character"),
+  startdata: string().required("StartDate is Required"),
+  enddata: string().required("EndDate is Required"),
+  degree : string().required("Degree is Required").min(20, "Must be at least 20 character").max(50, "Must be lower 50 character"),
+})
+export const ProfileQualificationValidation = object({
+  certificate: string().required("Certificate is Required").min(10, "Must be at least 10 character").max(50, "Must be lower 50 character"),
+  organization: string().required("Organization is Required").min(20, "Must be at least 20 character").max(120, "Must be lower 120 character"),
+  startdata: string().required("StartDate is Required"),
+  summary : string().required("Summary is Required").min(250, "Must be at least 250 character").max(1200, "Must be lower 1200 character"),
+  
+})
